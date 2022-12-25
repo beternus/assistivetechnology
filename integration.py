a01aa8ed719ba5ced3419d1a20d23d94898d472d
Bernardo <90293389+beternus@users.noreply.github.com>
2022-12-25 16:20:19 -0300

    Create integration.py

---
+++ integration.py
@@ -0,0 +1,44 @@
+/* def menu():
+  print("MENU")
+  choices={
+    1:"Call",
+    2:"Enable camera",
+    3: "Turn on/off light"
+  }
+  for i in choices.keys(): # Loop to print all choices and key of choice
+    print(f"[i] - choices[i]}"))
+  arg=int(input("Please Choose : "))
+  R = choices.get(arg,-1)
+  while R ==-1:
+    print("\n Wrong Choice ! Try again ...\n")
+    menu()
+  else:
+    print(f"You Chosed {R}")
+      if R==1:
+        chamadatelefone()
+      if R==2:
+        ligacamera()
+      if R=3:
+        ligaapagaluz()
+        
+  menu()  
+  
+  */
+  
+  def switch(v): yield lambda *c: v in c
+  
+  x = 3
+     for case in switch(x):
+      
+         if case (1):
+          # do something
+          break
+          
+          if case(2,4):
+          break
+          
+          if case(3):
+          break
+     else:
+          */
+          
